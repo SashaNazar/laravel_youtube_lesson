2e33c820f7b968867ea6a3a8144e5f65832e283a
alex <sashko.romanuk@gmail.com>
2026-02-23 19:12:58 +0200

    test commit in test_branch

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -22,4 +23,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+    public function comments(): HasMany
+    {
+
+    }
 }
+
